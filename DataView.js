a51557997eec144e7db7059b00b978887214feba
SydlWang <73863080+SydlWang@users.noreply.github.com>
2022-06-27 13:17:10 -0400

    Add files via upload
    added in a function to create a for loop doesn't quite work yet

--- DataView.js
+++ DataView.js
@@ -3,9 +3,31 @@ import React, { useState } from "react";
 export default class DataView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { data: this.props.entries };
+  }
+
+  countProps(obj) {
+    var count = 0;
+    for (var p in obj) {
+      obj.hasOwnProperty(p) && count++;
+    }
+    return count;
+  }
+
+  makeUl(array) {
+    let list = document.createElement("ul");
+
+    for (let i = 0; i < this.countProps(array); i++) {
+      let item = document.createElement("li");
+      item.appendChild(document.createTextNode(array[i]));
+      list.appendChild(item);
+    }
+    return list;
   }
 
   render() {
-    return <div></div>;
+    const { data } = this.state;
+    let uList = Object.values(this.makeUl(data));
+    return <div className="list">{uList}</div>;
   }
 }
